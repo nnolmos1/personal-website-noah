084deda47b742bd62f47e12e52a6620138889692
nnolmos1 <nnolmos1@gmail.com>
2026-09-08 23:00:49 -0500

    Hero & Navbar Start

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 export default function Hero() {
     return (
         <section id="hero" className="hero">
-            <h2> Hi, I'm Noah </h2>
-            <p> Desc </p>
+            <h2 id="hero-heading"> HI, I'M </h2>
+            <h2 id="hero-name"> Noah!</h2>
+            <p id="hero-description"> CS + Design @ UIC | Aspiring Developer & Designer </p>
+
+            <div id="hero-btns">
+                <button id="herobtn1" className="btn">View My Work</button>
+                <button id="herobtn2" className="btn">RESUME</button>
+            </div>
         </section>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 export default function Navbar() {
     return (
         <nav className="navbar">
+            <a href="#hero" id="hero-link">[ N.01 ]</a>
 
+            <div className="nav-links">
+                <a href="#about"> ABOUT </a>
+                <a href="#experience"> EXPERIENCE </a>
+                <a href="#projects"> PROJECTS </a>
+                <a href="#contact"> CONTACT </a>
+            </div>
         </nav>
     )
 }
