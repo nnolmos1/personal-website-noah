19f64167c984f968d33cbfdca315a33accc3aff4
nnolmos1 <nnolmos1@gmail.com>
2026-09-20 23:17:23 -0500

    Updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,17 @@
 export default function Contact() {
     return (
         <section id="contact" className="contact">
-            <h3 className="contact-heading"> 04 CONTACT </h3>
-            <p className="contact-text"> Feel free to reach out!! Always happy to meet new people and chat about development and design :) </p>
-            
-            <div className="contact-info">
-                <p className="contact-email"> nnolmos1@gmail.com </p>
+
+            <div className="contact-content">
+                <h3 className="contact-heading"> 04 CONTACT </h3>
+                <p className="contact-text"> Feel free to reach out!! Always happy to meet new people and chat about development and design :) </p>
+                
+                <div className="contact-info">
+                    <p className="contact-email"> nnolmos1@gmail.com </p>
+                </div>
+                
+                {/* <p className="faded"> Roblox: HiWintry </p> */}
             </div>
-            
-            {/* <p className="faded"> Roblox: HiWintry </p> */}
         </section>
     )
 }
